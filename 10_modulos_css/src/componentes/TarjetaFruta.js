61d9b4eea375d9e1c07d10c8edf3fe8b02a92d72
Tania Forero <brillyth.forero@gmail.com>
2020-07-10 11:07:29 -0500

    --- Auto-Git Commit ---

---
+++ 10_modulos_css/src/componentes/TarjetaFruta.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './TarjetaFruta.css'
+
+class TarjetaFruta extends React.Component {
+  state = {
+    cantidad: 0
+  }
+
+  agregar = () => {
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    })
+  }
+
+  quitar = () => {
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    })
+  }
+
+  limpiar = () => {
+    this.setState({
+      cantidad: 0
+    })
+  }
+
+  render () {
+    const hasItems = this.state.cantidad > 0
+    const clases = `card ${hasItems ? 'card-activa' : '' }`
+
+    return (
+      <div className={clases}> 
+        <h3>{ this.props.name }</h3>
+        <div>Cantidad: { this.state.cantidad }</div>
+        <button onClick={this.agregar}>+</button>
+        <button onClick={this.quitar}>-</button>
+        <button onClick={this.limpiar}>Limpiar</button>
+        <hr/>
+        <p>$ { this.props.price }</p>
+        <p>
+          Total: ${ this.props.price * this.state.cantidad }
+        </p>
+      </div>
+    )
+  }
+}
+
+export default TarjetaFruta
