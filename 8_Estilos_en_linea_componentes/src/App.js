6fc589226187dc045abc01c7c5b666fc819a991c
Tania Forero <brillyth.forero@gmail.com>
2020-07-10 08:57:59 -0500

    ajustes ejercicio 8

---
+++ 8_Estilos_en_linea_componentes/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TarjetaFruta from './componentes/TarjetaFruta'
+
+const App = () => (
+  <div>
+    <TarjetaFruta name={'Sandia'} price={5.00} />
+    <TarjetaFruta name={'Naranja'} price={1.50} />
+    <TarjetaFruta name='Kiwi' price={3.30} />
+  </div>
+)
+
+export default App
